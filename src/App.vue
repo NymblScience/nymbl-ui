5eab5fcc6a7610c85b9f2af5e39ef2584f68eaaf
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2019-03-28 15:45:53 +0200

    Removed unused SCSS components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
   </div>
 </template>
 
 <script>
 
 export default {
-  name: "app",
+  name: 'App',
 };
 </script>
 
