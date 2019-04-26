37683f086d2f92e693c2e0ce910feb8a14c4a787
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2019-04-26 12:42:42 +0300

    Updates

--- icons/Menu.vue
+++ icons/Menu.vue
@@ -13,33 +13,63 @@
       :height="size"
       viewBox="0 0 24 24"
     >
-      <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z">
-        <title>{{ title }}</title>
-      </path>
+      <g transform="translate(-105.24 -61.446)">
+        <g
+          transform="matrix(.64661 0 0 .64661 -74.249 -105.93)"
+          fill-rule="evenodd"
+          stroke-width=".67193"
+        >
+          <rect
+            transform="matrix(-1 -.0019925 .0027971 1 0 0)"
+            x="-311.43"
+            y="258.29"
+            width="34.571"
+            height="5.8891"
+            rx="3.053"
+          />
+          <rect
+            transform="matrix(-1 -.0019925 .0027971 1 0 0)"
+            x="-311.4"
+            y="268.64"
+            width="34.571"
+            height="5.8891"
+            rx="3.053"
+          />
+          <rect
+            transform="matrix(-1 -.0019925 .0027971 1 0 0)"
+            x="-311.37"
+            y="279.29"
+            width="34.571"
+            height="5.8891"
+            rx="3.053"
+          />
+        </g>
+      </g>
+      <title>{{ title }}</title>
     </svg>
   </span>
 </template>
 
 <script>
 export default {
-  name: 'MenuIcon',
+  name: "MenuIcon",
   props: {
     title: {
       type: String,
-      default: 'Menu icon',
+      default: "Menu"
     },
     decorative: {
       type: Boolean,
-      default: false,
+      default: false
     },
     fillColor: {
       type: String,
-      default: 'currentColor',
+      default: "currentColor"
     },
     size: {
       type: Number,
-      default: 24,
-    },
-  },
+      default: 24
+    }
+  }
 };
 </script>
