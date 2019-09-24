46a7bdd32d9b92a0fd88aba7d48ed89eb449f0c0
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2019-09-24 13:59:26 +0300

    Update Color variable system

--- src/style/Colors.vue
+++ src/style/Colors.vue
@@ -2,34 +2,52 @@
   <div class="color-container">
     <div v-for="color in colors" :key="color" class="color">
       <div :style="{ background: color }" class="color-block"></div>
-      <div class="color-code" title="Copy to clipboard">{{ color }}</div>
+      <div class="color-code">{{ color }}</div>
     </div>
   </div>
 </template>
 <script>
+import colors from "@/assets/sass/colors.scss";
+
 export default {
   name: "Colors",
-  data() {
-    return {
-      colors: [
-        "#0C6A69",
-        "#09B3B0",
-        "#83EAE7",
-        "#CCFEFF",
-        "#244F6F",
-        "#4B88B5",
-        "#94BFDF",
-        "#C2E2F9",
-        "#5E3D2A",
-        "#8E6853",
-        "#A79184",
-        "#D9C5BA",
-        "#292F2F",
-        "#595959",
-        "#A2A2A2",
-        "#BCBCBC"
-      ]
-    };
+  computed: {
+    colors() {
+      return Object.values(colors);
+    }
+  },
+  // data() {
+  //   return {
+  //     colors: [
+  //       "#0C6A69",
+  //       "#09B3B0",
+  //       "#83EAE7",
+  //       "#CCFEFF",
+  //       "#244F6F",
+  //       "#4B88B5",
+  //       "#94BFDF",
+  //       "#C2E2F9",
+  //       "#5E3D2A",
+  //       "#8E6853",
+  //       "#A79184",
+  //       "#D9C5BA",
+  //       "#292F2F",
+  //       "#595959",
+  //       "#A2A2A2",
+  //       "#BCBCBC",
+  //       "#BB5F15",
+  //       "#E2771F",
+  //       "#EDAB6F",
+  //       "#F8D8B9",
+  //       "#751316",
+  //       "#AC1A1F",
+  //       "#D47F80",
+  //       "#F5D0D0"
+  //     ]
+  //   };
+  // },
+  created() {
+    console.log("variables :", colors);
   }
 };
 </script>
