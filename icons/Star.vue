c9d4212ea4e16fd24f9c65ae9e1e0809289e6a2c
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2019-09-24 14:00:29 +0300

    Updates

--- icons/Star.vue
+++ icons/Star.vue
@@ -13,7 +13,9 @@
       :height="size"
       viewBox="0 0 24 24"
     >
-      <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z">
+      <path
+        d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+      >
         <title>{{ title }}</title>
       </path>
     </svg>
@@ -22,24 +24,24 @@
 
 <script>
 export default {
-  name: 'StarIcon',
+  name: "StarIcon",
   props: {
     title: {
       type: String,
-      default: 'Star icon',
+      default: "Star icon"
     },
     decorative: {
       type: Boolean,
-      default: false,
+      default: false
     },
     fillColor: {
       type: String,
-      default: 'currentColor',
+      default: "currentColor"
     },
     size: {
       type: Number,
-      default: 24,
-    },
-  },
+      default: 24
+    }
+  }
 };
 </script>
