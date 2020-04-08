96a8684c2caa4371e36b30797325eb80657378ca
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2020-04-08 16:15:38 +0200

    Add n-table expand style

--- src/components/NTableRowExpand.vue
+++ src/components/NTableRowExpand.vue
@@ -2,11 +2,15 @@
   <div
     :class="{ 'is-expanded': expandedRows.includes(id) }"
     class="n-table-row__expanded"
-    :style="{ 'margin-left': expandWidth + 'px' }"
   >
     <transition-expand>
       <div v-if="expandedRows.includes(id)">
-        <slot />
+        <div
+          class="n-table-row__expanded-container"
+          :style="{ 'padding-left': expandWidth + 'px' }"
+        >
+          <slot />
+        </div>
       </div>
     </transition-expand>
   </div>
@@ -39,6 +43,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/sass/colors.scss";
 .expand-enter-active,
 .expand-leave-active {
   transition: height 300ms ease-in-out;
@@ -46,9 +51,13 @@ export default {
 }
 .n-table-row__expanded {
   flex-basis: 100%;
-
+  cursor: unset;
   &:hover {
     background: #fff;
   }
+  &-container {
+    border-left: 2px solid $green-2;
+  }
+  padding-bottom: 5px;
 }
 </style>
