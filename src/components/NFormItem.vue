6cb7207986c0355b2b64717dbc2fc1647b024610
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2020-06-03 13:04:22 +0200

    Add label to NFormItem

--- src/components/NFormItem.vue
+++ src/components/NFormItem.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="n-form__item">
+    <div v-if="label" class="n-form__item-label">{{ label }}</div>
     <slot />
   </div>
 </template>
@@ -19,5 +20,11 @@ export default {
 <style lang="scss">
 .n-form__item {
   padding-bottom: 0.7rem;
+  &-label {
+    font-size: 0.85rem;
+    font-weight: 500;
+    letter-spacing: 0.03rem;
+    color: #292f2f;
+  }
 }
 </style>
