d1364ecb3985f80f417a29599a59136905d49952
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2020-11-09 11:19:49 +0200

    Update color example

--- src/Colors.vue
+++ src/Colors.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="my-10 w-100 Colors">
-    <a id="colors"  class="anchor"></a>
+    <a id="colors"  class="anchor "></a>
       <h3 class="text-4xl my-10">Colors</h3>
-      <div class="flex" v-for="color in colors" :key="colors[color]">
+
+    <div class="flex" v-for="color in colors" :key="colors[color]">
+      <div
+        class="mb-3 text-center text-sm uppercase "
+        v-for="shade in getColors(color)"
+        :key="shade"
+        @click="handleColorClick(shade)"
+      >
         <div
-          class="mb-3  text-center text-sm uppercase "
-          v-for="shade in getColors(color)"
-          :key="shade"
-          @click="handleColorClick(shade)"
-        >
-          <div
-            class="mb-1 mr-1 cursor-pointer hover:shadow-lg"
-            style="width: 6rem; height: 6rem"
-            :style="`background: ${shade}`"
-          ></div>
-          {{ shade }}
-        </div>
+          class="mb-1 mr-1 cursor-pointer hover:shadow-lg"
+          style="width: 6rem; height: 6rem"
+          :style="`background: ${shade}`"
+        ></div>
+        {{ shade }}
       </div>
+    </div>
+
     </div>
 
 </template>
@@ -34,7 +36,7 @@ export default {
     getColors(color) {
       const colors = [];
       Object.keys(color).forEach((key) => colors.push(color[key]));
-      return colors.filter((shade) => shade.charAt(0) === '#');
+      return colors.filter((shade) => shade.charAt(0) === '#').filter((shade) => shade !== '#d9d9d9');
     },
 
     handleColorClick(color) {
