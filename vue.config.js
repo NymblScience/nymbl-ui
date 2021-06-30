8b1c3b84b92e7e143ea45dcb805034e50845b0a4
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2021-06-30 21:08:00 +0300

    Upgrade to Vue3

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,22 @@ module.exports = {
     devtool: "source-map"
   },
   chainWebpack: config => {
+    config.resolve.alias.set("vue", "@vue/compat");
+
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap(options => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        };
+      });
+
     config.module
       .rule("eslint")
       .use("eslint-loader")
