b4e61d65d04305ff00034aa0f17d3c30e98ff488
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2022-01-06 16:45:10 +0200

    Add default shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,7 +45,7 @@ module.exports = {
         extrabold: 700,
       },
       boxShadow: {
-        default: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 1px 4px rgba(0, 0, 0, 0.25)',
+        DEFAULT: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 1px 4px rgba(0, 0, 0, 0.25)',
       },
       lineHeight: {
         3: '0.9583rem', // 23px
