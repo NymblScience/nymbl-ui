d4b78cd225d3bba6c8120f8c73e0e8311a3b93ec
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2022-01-12 15:45:28 +0200

    Remove compat build

--- vue.config.js
+++ vue.config.js
@@ -5,38 +5,25 @@ module.exports = {
   publicPath: "/nymbl-ui/",
   devServer: {
     open: true,
-    port: 8089
+    port: 8089,
   },
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        APP_VER: JSON.stringify(require("./package.json").version)
-      })
+        APP_VER: JSON.stringify(require("./package.json").version),
+      }),
     ],
-    devtool: "source-map"
+    devtool: "source-map",
   },
-  chainWebpack: config => {
-    config.resolve.alias.set("vue", "@vue/compat");
-
+  chainWebpack: (config) => {
     config.module
       .rule("vue")
       .use("vue-loader")
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        };
-      });
 
-    config.module
       .rule("eslint")
       .use("eslint-loader")
       .options({
-        fix: true
+        fix: true,
       });
-  }
+  },
 };
